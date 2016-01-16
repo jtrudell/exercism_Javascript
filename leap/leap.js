86392f3443b6a48d07ee1c8a8f2efc1f77f6b7f4
Jen Trudell <jtrudell@gmail.com>
2016-01-15 21:36:17 -0600

    refactor

--- leap/leap.js
+++ leap/leap.js
@@ -1,12 +1,17 @@
 function Year(year) {
+
   this.year = year;
   this.isLeap = function() {
     if (this.year % 4 === 0) {
-      if ( (this.year % 100 !== 0) || (this.year % 100 === 0 && this.year % 400 === 0)) {
-        return true;
-      } else {
-        return false;
-      }
+      return checkYear(this.year);
+    } else {
+      return false;
+    }
+  };
+
+  checkYear = function(year) {
+    if ( (year % 100 !== 0) || (year % 400 === 0) ) {
+      return true;
     } else {
       return false;
     }
