900a7cea2b62b369a194f7804a83f026be784f7f
Jen Trudell <jtrudell@gmail.com>
2016-01-30 08:57:29 -0600

    initial solution

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,11 +1,31 @@
-var anagram = function(text) {
-	var matches = function(arrray) {
-		if (text !== arrray) {
-		return [];
-}
-	}
-
+var Anagram = function(word) {
+	this.originalWord = word.toLowerCase();
+	this.sortedWord = word.toLowerCase().split('').sort();
 };
 
-module.exports = anagram;
-      
+Anagram.prototype.matches = function(list) {
+	if (arguments.length == 1) {
+		list = typeof list === 'string' ? [list] : list;
+	} else {
+		var args = [];
+		for (var j = 0; j < arguments.length; j++) {
+			args[j] = arguments[j];
+		}
+		list = args;
+	}
+	var matchList = [];
+	var word = this.sortedWord.join('');
+
+
+	for (var i = 0; i < list.length; i++) {
+		if (list[i].toLowerCase() !== this.originalWord) {
+			var sortedListWord = list[i].toLowerCase().split('').sort();
+			if (sortedListWord.join('') === word) {
+				matchList.push(list[i]);
+			}
+		}
+	}
+	return matchList;
+};
+
+module.exports = Anagram;
