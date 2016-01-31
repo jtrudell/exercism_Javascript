6ee50125878c8e91445dbb36bdc072396b80f573
Jen Trudell <jtrudell@gmail.com>
2016-01-31 09:37:21 -0600

    initial solution

---
+++ grade-school/grade-school.js
@@ -0,0 +1,28 @@
+var School = function() {
+  this.allStudents = {};
+  
+  this.roster = function() {
+    return this.allStudents;
+  };
+
+  this.add = function(name, grade) {
+    var allStudents = this.allStudents;
+    if (allStudents[grade]) {
+      allStudents[grade].push(name);
+      allStudents[grade] = allStudents[grade].sort();
+    } else {
+      allStudents[grade] = [name];
+    }
+  };
+
+  this.grade = function(number) {
+    if (this.allStudents[number]) {
+      return this.allStudents[number];
+    } else {
+      return [];
+    }
+  };
+
+};
+
+module.exports = School;
