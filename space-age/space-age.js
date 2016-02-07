0072abacfda46aa878abdebd5b0e00eaba2aa689
Jen Trudell <jtrudell@gmail.com>
2016-02-07 13:46:23 -0600

    adds factor for planet functions

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,18 +1,15 @@
+var planets = {
+      "Mercury": 0.2408467,
+      "Venus": 0.61519726,
+      "Mars": 1.8808158,
+      "Jupiter": 11.862615,
+      "Saturn": 29.447498,
+      "Uranus": 84.016846,
+      "Neptune": 164.79132
+    };
 
 var SpaceAge = function(age) {
-  'use strict';
-  
   this.seconds = age;
-  this.planets = {
-    "mercury": 0.2408467,
-    "venus": 0.61519726,
-    "mars": 1.8808158,
-    "jupiter": 11.862615,
-    "saturn": 29.447498,
-    "uranus": 84.016846,
-    "neptune": 164.79132
-  };
-
   this.onEarth = function() {
     var secondsInDay = 86400;
     var earth = (age / (365.25 * secondsInDay)).toFixed(2);
@@ -20,37 +17,19 @@ var SpaceAge = function(age) {
   };
 
   this.convert = function(planet) {
-    var total = (this.onEarth() / this.planets[planet]).toFixed(2);
+    var total = (this.onEarth() / planets[planet]).toFixed(2);
     return parseFloat(total);
   };
-
-  this.onMercury = function() {
-    return this.convert("mercury");
-  };
-
-  this.onVenus = function() {
-    return this.convert("venus");
-  };
-
-  this.onMars = function() {
-    return this.convert("mars");
-  };
-
-  this.onJupiter = function() {
-    return this.convert("jupiter");
-  };
-
-  this.onSaturn = function() {
-    return this.convert("saturn");
-  };
-
-  this.onUranus = function() {
-    return this.convert("uranus");
-  };
-
-  this.onNeptune = function() {
-    return this.convert("neptune");
-  };
 };
+
+function planetGenerator(planet) {
+  SpaceAge.prototype["on" + planet] = function() {
+    return this.convert(planet);
+  };
+}
+
+for (var planet in planets) {
+  planetGenerator(planet);
+}
 
 module.exports = SpaceAge;
